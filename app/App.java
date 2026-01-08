8e4e96ce098f8a43c2d7934cee71cc917844ee9b
Gabriel-Devweb <bielsilvabarreto2012@gmail.com>
2026-01-08 03:06:52 -0300

    Fazendo a interface

--- app/App.java
+++ app/App.java
@@ -1,14 +1,41 @@
 package app;
+
 import src.Cpf;
+import src.CpfValidador;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String args[]) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Digite o CPF");
-    String cpfDigitado = scanner.nextLine();
-    Cpf cpf = new Cpf();
-    cpf.CpfLimpar(cpfDigitado);
-    scanner.close();
-  }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o CPF:");
+        String cpfDigitado = scanner.nextLine();
+
+        Cpf cpf = new Cpf();
+        cpf.cpfLimpar(cpfDigitado);
+
+        if (!cpf.tamanhoValido()) {
+            System.out.println("CPF inválido");
+            scanner.close();
+            return;
+        }
+
+        if (cpf.temDigitosRepetidos()) {
+            System.out.println("CPF inválido");
+            scanner.close();
+            return;
+        }
+
+        CpfValidador validador = new CpfValidador();
+        boolean valido = validador.validar(cpf.getCpfLimpo());
+
+        if (valido) {
+            System.out.println("CPF válido");
+        } else {
+            System.out.println("CPF inválido");
+        }
+
+        scanner.close();
+    }
 }
